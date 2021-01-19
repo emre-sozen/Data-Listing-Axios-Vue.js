53e49ea457fe71271b3f7bb13d89cc3850616196
Emre Sözen <szn.emre1@gmail.com>
2021-01-19 22:56:54 +0300

    Add a project file

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+    <Users />
+
+  </div>
+</template>
+
+<script>
+
+import Users from './components/Users.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Users
+  }
+}
+</script>
+
+<style>
+
+</style>
